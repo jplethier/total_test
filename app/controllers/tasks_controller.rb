a0c8fe3cf995ee941c38753ac4dc0dce0c897200
João Paulo Lethier <jplethier@gmail.com>
2014-01-25 13:58:33 -0200

    adds load_and_authorize resource for tasks_controller; changes the application layout to use haml instead of erb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  def index
+  load_and_authorize_resource
 
+  def index
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_messages
+    flash.collect do |key, msg|
+      content_tag :div, class: "message #{key}" do
+        content_tag(:p, msg)
+      end
+    end.join.html_safe
+  end
 end
