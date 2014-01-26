abcf9d66d247b806d1083ca082827fd0d6a67321
João Paulo Lethier <jplethier@gmail.com>
2014-01-26 12:13:53 -0200

    creating tasks correctly via js

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,28 @@
 class TasksController < ApplicationController
   load_and_authorize_resource
 
+  prepend_before_filter do
+    params[:task] &&= task_params
+  end
+
   def index
+    @new_task = current_user.tasks.build
+  end
+
+  def create
+    @task.user = current_user
+    if @task.save
+      flash.now[:notice] = 'Task created'
+      render :task_created
+    else
+      flash.now[:error] = 'Could not create the task'
+      render :task_error
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :due_date, :priority)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tasks
+  accepts_nested_attributes_for :tasks, allow_destroy: true
 
   def guest?
     self.new_record?
